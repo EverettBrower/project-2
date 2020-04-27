d720db0f553c6edb25b2d47871fce5273edf5a6f
Everett Brower <everettb@uoregon.edu>
2020-04-27 15:56:09 -0700

    Finished functions.js requirements for revWords 1,2,3

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -15,8 +15,44 @@ let containsDuplicates = s => {
   return false;
 };
 
+//Reverse a single words function
+let reverseWord = word => {
+  var newString = "";
+  for (var i = word.length - 1; i >= 0; i--) {
+    newString += word[i];
+  }
+  return newString;
+};
+
 //RevWords1
 
-revWords = rev => {
-  for()
-}
+let revWords1 = sentence => {
+  let wordsArr = sentence.split(" ");
+  let result = " ";
+  for (var i = 0; i < wordsArr.length; i++) {
+    result = result + reverseWord(wordsArr[i] + " ");
+  }
+  return result;
+};
+
+//revWords2
+
+let revWords2 = sentence => {
+  let wordsArr = sentence.split(" ");
+  let result = " ";
+  wordsArr.forEach(word => {
+    result = result + reverseWord(word) + " ";
+  });
+  return result;
+};
+
+//revWords3
+
+let revWords3 = sentence => {
+  let wordsArr = sentence.split(" ");
+  let result = " ";
+  for (var word of wordsArr) {
+    result = result + reverseWord(word) + " ";
+  }
+  return result;
+};
